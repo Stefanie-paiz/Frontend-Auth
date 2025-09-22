c34f358cbcd5a608f4ab753e7baec22b11eb30b8
StefaniePaiz <ps25001@esfe.agape.edu.sv>
2025-09-22 13:24:15 -0600

    codificardo

--- FrontAuth.WebApp/Controllers/HomeController.cs
+++ FrontAuth.WebApp/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
-using System.Diagnostics;
 using FrontAuth.WebApp.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace FrontAuth.WebApp.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
--- FrontAuth.WebApp/Program.cs
+++ FrontAuth.WebApp/Program.cs
@@ -1,12 +1,30 @@
+using FrontAuth.WebApp.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddHttpClient<ApiService>(client =>
+{
+    // Defino la URL base de la API que vamos a consumir
+    client.BaseAddress = new Uri("https://localhost:7035/api/"); // API base (puerto cambia segun pc)
+});
+
+builder.Services.AddScoped<AuthService>();
+
+//Configuracion de la autenticacion de la aplicacion usando cookies
+builder.Services.AddAuthentication("AuthCookie")
+    .AddCookie("AuthCookie", options =>
+    {
+        options.LoginPath = "/Auth/Login";
+        options.ExpireTimeSpan = TimeSpan.FromMinutes(60); // Despues de 60 minutos, el usuario tendra que iniciar sesion nuevamente
+    });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
@@ -18,6 +36,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
